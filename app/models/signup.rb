9aa37f9b4399d118aaeb0906329ecbd59dde03da
Andrew Drummond <asdrummo@gmail.com>
2011-10-01 12:43:32 -0400

    iOS remove

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,5 +1,5 @@
 class Signup < ActiveRecord::Base
-  attr_accessible :email
+  attr_accessible :email, :name
   
   EMAIL_REGEX = /^[A-ZA-Z0-9._%+-]+@[A-ZA-Z0-9.-]+\.[A-Z]{2,4}$/i
   validates :email, :presence => true, :length => {:maximum => 100 }, :format => EMAIL_REGEX, :confirmation => true, :uniqueness => true  
